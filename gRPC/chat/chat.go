61c0972fa3a9fefac4c98ffabd577d86b441e695
Fernando Da Rós <fernando.ros@stone.com.br>
2020-10-31 11:53:49 -0300

    Add comment

--- gRPC/chat/chat.go
+++ gRPC/chat/chat.go
@@ -9,6 +9,8 @@ import (
 type Server struct {
 }
 
+// Where is the link between this application and chat.pb.go?
+
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
 	log.Printf("Received message body from client: %s", message.Body)
 	return &Message{Body: "Hello From the Server!"}, nil
